45c9653976930a96022cfd6cf772ba0d0d1574bb
woojin <jwjley@naver.com>
2023-03-18 14:13:49 +0900

    feat : 프로젝트 생성 (2023-03-18)

---
+++ src/main/java/kr/spring/miraclestep_web/MiracleStepWebApplication.java
@@ -0,0 +1,13 @@
+package kr.spring.miraclestep_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracleStepWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiracleStepWebApplication.class, args);
+    }
+
+}
